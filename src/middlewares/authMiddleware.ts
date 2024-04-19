3c8ea7fceb9b8deb466b8e1065d6eba2c9887b0f
Salah Eddine Naouassih <salahnaouassih10@gmail.com>
2024-04-19 10:52:57 +0200

    remove token

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -16,6 +16,7 @@ function authenticateToken(
   const authHeader =
     req.headers["authorization"] || (req.headers["Authorization"] as string);
   const token = authHeader && authHeader.split(" ")[1];
+  console.log(token);
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, getConfig().ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
